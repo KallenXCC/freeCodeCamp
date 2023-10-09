de3230bf60bbc7c41825f6ee1455404c517732c0
Braeton Buckley <braetonbuckley@gmail.com>
2023-10-09 13:16:49 -0700

    Debugging and Lesson Logs

---
+++ JavaScript Algorithms and Data Structures/Debugging/console.js
@@ -0,0 +1,21 @@
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three);
+console.clear();
+
+/*Debugging Lessons:
+Use the JavaScript Console to Check the Value of a Variable
+Understanding the Differences between the freeCodeCamp and Browser Console
+Use typeof to Check the Type of a Variable
+Catch Misspelled Variable and Function Names
+Catch Unclosed Parentheses, Brackets, Braces and Quotes
+Catch Mixed Usage of Single and Double Quotes
+Catch Use of Assignment Operator Instead of Equality Operator
+Catch Missing Open and Closing Parenthesis After a Function Call
+Catch Arguments Passed in the Wrong Order When Calling a Function
+Catch Off By One Errors When Using Indexing
+Use Caution When Reinitializing Variables Inside a Loop
+Prevent Infinite Loops with a Valid Terminal Condition*/
--- JavaScript Algorithms and Data Structures/Regular Expressions/replace.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions/replace.js
@@ -2,3 +2,38 @@ let hello = "   Hello, World!  ";
 let wsRegex = /^(\s+)(.*?)(\s+)$/; // find whitespace
 let result = hello.replace(wsRegex, '$2'); // removes whitespace
 console.log(result);
+
+/*Regular Expressions Lessons:
+Using the Test Method
+Match Literal Strings
+Match a Literal String with Different Possibilities
+Ignore Case While Matching
+Extract Matches
+Find More Than the First Match
+Match Anything with Wildcard Period
+Match Single Character with Multiple Possibilities
+Match Letters of the Alphabet
+Match Numbers and Letters of the Alphabet
+Match Single Characters Not Specified
+Match Characters that Occur One or More Times
+Match Characters that Occur Zero or More Times
+Find Characters with Lazy Matching
+Find One or More Criminals in a Hunt
+Match Beginning String Patterns
+Match Ending String Patterns
+Match All Letters and Numbers
+Match Everything But Letters and Numbers
+Match All Numbers
+Match All Non-Numbers
+Restrict Possible Usernames
+Match Whitespace
+Match Non-Whitespace Characters
+Specify Upper and Lower Number of Matches
+Specify Only the Lower Number of Matches
+Specify Exact Number of Matches
+Check for All or None
+Positive and Negative Lookahead
+Check For Mixed Grouping of Characters
+Reuse Patterns Using Capture Groups
+Use Capture Groups to Search and Replace
+Remove Whitespace from Start and End*/
